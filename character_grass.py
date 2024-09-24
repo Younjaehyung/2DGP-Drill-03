31ad125051361c89eab1227b08b873fc511dde4d
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-09-24 11:56:29 +0900

    뼈대 완성

---
+++ character_grass.py
@@ -0,0 +1,25 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+#grass.draw_now(400,30)
+#character.draw_now(400,90)
+
+
+def run_circle():
+    pass
+
+def run_rectangle():
+    pass
+
+while True:
+    run_rectangle()
+    run_circle()   
+
+
+
+close_canvas()
